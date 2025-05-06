09b0357fe20b1cccc5684465058441688d08824a
PauloCesar53 <pcdilauro53@gmail.com>
2025-05-05 22:13:56 -0300

    Add: task disppay

--- main.c
+++ main.c
@@ -19,7 +19,7 @@
 #define botaoA 5//definindo botão A
 bool modo=1;//Flag para modo (1 diurno e 0 noturno)
 static volatile uint32_t last_time_A = 0; // Armazena o tempo do último evento para Bot B(em microssegundos)
-
+uint Cor_sinal=4;//auxiliar para com do sinal (Verde=1, Amarelo=2, Vermelho=3)
 void vLedRGBTask()//task para LED RGB
 {
     //definindo LED verde
@@ -35,12 +35,20 @@ void vLedRGBTask()//task para LED RGB
     {   
         if(modo){//modo diurno
             for(int i=0;i<4;i++){//para verde (total 4 segundos)
+            Cor_sinal=1;
                 gpio_put(led1, true);
                 vTaskDelay(pdMS_TO_TICKS(500));//beep curto por segundo 
                 gpio_put(led1, false);
                 vTaskDelay(pdMS_TO_TICKS(500));
+                if(modo==0){
+                    break;
+                }
             }
             for(int i=0;i<10;i++){//para amarelo (Vermelho e verde ligados simultaneamente)(total 4 segundos)
+                Cor_sinal=2;
+                if(modo==0){
+                    break;
+                }
                 gpio_put(led1, true);
                 gpio_put(led3, true);
                 vTaskDelay(pdMS_TO_TICKS(200));//beep rápido intermitente 
@@ -49,12 +57,17 @@ void vLedRGBTask()//task para LED RGB
                 vTaskDelay(pdMS_TO_TICKS(200));
             }
             for(int i=0;i<2;i++){//para vermelho (total 4 segundos)
+            Cor_sinal=3;
+                if(modo==0){
+                    break;
+                }
                 gpio_put(led3, true);
                 vTaskDelay(pdMS_TO_TICKS(500));//tom continuo curto 
                 gpio_put(led3, false);
                 vTaskDelay(pdMS_TO_TICKS(1500));
             }
         }else{//modo noturno (amarelo lento a cada 2s )
+            Cor_sinal=2;
             gpio_put(led1, true);
             gpio_put(led3, true);
             vTaskDelay(pdMS_TO_TICKS(1500));
@@ -95,24 +108,40 @@ void vDisplay3Task()
     ssd1306_fill(&ssd, false);
     ssd1306_send_data(&ssd);
 
-    char str_y[5]; // Buffer para armazenar a string
-    int contador = 0;
+    char str_y[8]={'D','I','U','R','N','O','\0'}; // Buffer para armazenar a string
+    char str_x[8]={'N','O','T','U','R','N','O','\0'};
+    char str_R[6]="pare\0";//mensagem sinal vermelho
+    char str_Y[9]="atencao\0";//mensagem sinal amarelo
+    char str_G[17]="pode atravessar\0";//mensagem sinal verde
+    //int contador = 0;
     bool cor = true;
+    bool aux=1;//efeito visual mensagem 
     while (true)
     {
-        sprintf(str_y, "%d", contador); // Converte em string
-        contador++;                     // Incrementa o contador
+        //sprintf(str_y, "%d", contador); // Converte em string
+        //contador++;                     // Incrementa o contador
         ssd1306_fill(&ssd, !cor);                          // Limpa o display
         ssd1306_rect(&ssd, 3, 3, 122, 60, cor, !cor);      // Desenha um retângulo
         ssd1306_line(&ssd, 3, 25, 123, 25, cor);           // Desenha uma linha
         ssd1306_line(&ssd, 3, 37, 123, 37, cor);           // Desenha uma linha
-        ssd1306_draw_string(&ssd, "CEPEDI   TIC37", 8, 6); // Desenha uma string
-        ssd1306_draw_string(&ssd, "EMBARCATECH", 20, 16);  // Desenha uma string
-        ssd1306_draw_string(&ssd, "  FreeRTOS", 10, 28); // Desenha uma string
-        ssd1306_draw_string(&ssd, "Contador  LEDs", 10, 41);    // Desenha uma string
-        ssd1306_draw_string(&ssd, str_y, 40, 52);          // Desenha uma string
+        ssd1306_draw_string(&ssd, "SEMAFORO", 29, 6); // Desenha uma string
+        ssd1306_draw_string(&ssd, "INTELIGENTE", 20, 16);  // Desenha uma string
+        
+            
+        if(Cor_sinal==1){//mensagem se verde
+            ssd1306_draw_string(&ssd, str_G, 4, 28); // Desenha uma string
+        }else if(Cor_sinal==2){//mensagem se amarelo
+            ssd1306_draw_string(&ssd, str_Y, 35, 28); // Desenha uma string
+        }else if(Cor_sinal==3){//mensagem se vermelhho
+            ssd1306_draw_string(&ssd, str_R, 41, 28); // Desenha uma string
+        }
+        ssd1306_draw_string(&ssd, "MODO", 45, 41);    // Desenha uma string
+        if(modo){
+            ssd1306_draw_string(&ssd, str_y, 38, 52);          // Desenha uma string
+        }else{
+            ssd1306_draw_string(&ssd, str_x, 33, 52);
+        }
         ssd1306_send_data(&ssd);                           // Atualiza o display
-        sleep_ms(735);
     }
 }
 
@@ -127,6 +156,7 @@ void vGpio_irq_handler(uint gpio, uint32_t events)//task para botões
         reset_usb_boot(0, 0);
     }
     if(gpio_get(botaoA)==0 &&(current_time - last_time_A) > 200000){//200ms de boucing adiconado como condição
+        last_time_A = current_time; // Atualiza o tempo do último evento
         modo=!modo;
     }
 }
